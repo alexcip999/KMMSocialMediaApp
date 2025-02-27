b287a47e13f0c077dd2571bdc9e8b26c48301e59
alexcip999 <58331044+alexcip999@users.noreply.github.com>
2025-02-27 22:27:34 +0200

    Social Media App - start SignUpScreen

---
+++ androidApp/src/main/java/com/example/kmmsocialmediaapp/android/auth/signup/SignUpScreen.kt
@@ -0,0 +1,15 @@
+package com.example.kmmsocialmediaapp.android.auth.signup
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SignUpScreen(
+    modifier: Modifier = Modifier,
+    uiState: SignUpUiState,
+    onUsernameChange: (String) -> Unit,
+    onEmailChange: (String) -> Unit,
+    onPasswordChange: (String) -> Unit
+){
+
+}
